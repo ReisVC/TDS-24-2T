3552743861d0516b1e56f92a2c138983a4954b6e
cintiakuhn66 <vitor.reisvc@gmail.com>
2024-08-13 14:04:32 -0300

    Atualizações arquivos

---
+++ Aulas/Aula3/index.js
@@ -0,0 +1,34 @@
+// console.log("Hello World");
+
+// const nome = 'Vitor';
+// const sobrenome = 'Reis';
+// const idade = 21;
+// const souEstudante = true;
+
+// console.log('Nome:', nome, sobrenome,
+//     'Idade:',idade,
+//     'Sou Estudante:',souEstudante 
+// )
+
+// const got = "Game of Thrones"
+// const temporadasDeGot = 8;
+
+// console.log(typeof got)
+// console.log(typeof temporadasDeGot)
+
+// // return typeof temporadasDeGot
+
+// let novaVariavel
+// console.log(typeof novaVariavel)
+
+// novaVariavel = 2;
+// console.log(typeof novaVariavel)
+
+// novaVariavel = undefined
+// console.log(typeof novaVariavel)
+
+$nome = prompt("Qual seu nome?");
+$idade = Number(prompt("Qual sua idade?"));
+
+console.log(typeof $nome)
+console.log(typeof $idade)
