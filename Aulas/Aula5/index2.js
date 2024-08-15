a6e21826f80310981e0f3e777ba7cb708653c647
cintiakuhn66 <vitor.reisvc@gmail.com>
2024-08-15 17:08:59 -0300

    Att index2

---
+++ Aulas/Aula5/index2.js
@@ -0,0 +1,9 @@
+let perg1 = "Você está tendo um bom dia?"
+let perg2 = "Você vai ir na academia hoje?"
+let perg3 = "Você vai às 19h?"
+
+let dia = prompt(perg1)
+let academia = prompt(perg2)
+let horas = prompt(perg3)
+
+console.log(perg1, dia,"\n",perg2, academia,"\n",perg3, horas)
