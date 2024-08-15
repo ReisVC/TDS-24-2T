38a2f502928dd669239f786a6466912d936aa109
cintiakuhn66 <vitor.reisvc@gmail.com>
2024-08-15 14:33:50 -0300

    Atividade de Revisão

--- Aulas/Aula5/index.js
+++ Aulas/Aula5/index.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+let a = 10
+let b = 10
+
+console.log(b) // Saída é 10
+
+b = 5 // Variável b passa a valer 5
+
+console.log(a, b) // Imprime os valores 10 e 5
+
+
+
+
+let c = 10
+let d = 20
+e = c // Variável c tem o valor de 10
+d = e // Variável b passa a valer 10
+c = d // Variável a passa a valer 10
+
+console.log(c, d, e) // Imprime os valores 10, 10 e 10
