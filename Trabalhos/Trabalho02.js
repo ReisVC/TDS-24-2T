896f40aeecac624e88458015f91dbc0e2cebb7a0
cintiakuhn66 <vitor.reisvc@gmail.com>
2024-09-17 16:50:08 -0300

    Trabalho02

---
+++ Trabalhos/Trabalho02.js
@@ -0,0 +1,38 @@
+const personagem = {
+    nome: '',
+    classe: '',
+    nivel: 1,
+    hp: 10,
+    habilidade: []
+}
+
+const cadastro = () => {
+  let nomeUsuario = prompt('Qual nome do seu personagem?')
+  let classe = prompt('Qual a classe do seu personagem?')
+
+}
+
+const vilanesco = {
+    nome: 'Sauron',
+    classe: 'Feiticeiro',
+    nivel: 100,
+    hp: 100,
+    habilidade: 'Manipular'
+}
+
+let ataque = true
+let defesa = false
+
+const atacar = () => {
+    if(ataque) {
+
+    }
+}
+
+const defender = () => {
+    
+}
+
+const ataqueEspecial = () => {
+    
+}
